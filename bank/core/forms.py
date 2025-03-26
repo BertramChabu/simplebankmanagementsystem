4ccf75d00ae5fe911ad53967b16453381aeec202
mellokidnova <145387124+BertramChabu@users.noreply.github.com>
2025-03-27 02:02:12 +0300

    uodate

--- bank/core/forms.py
+++ bank/core/forms.py
@@ -5,8 +5,4 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['name', 'email', 'phone']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
-            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone number'}),
-        }
+        
